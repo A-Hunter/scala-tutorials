30c9b8f74b336cfe95ef0d1e3360b19b47faecd3
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2018-10-25 22:20:42 +0100

    function and procedure

---
+++ src/main/scala/com/scala/tutorials/d/functions/procedures/Demo.scala
@@ -0,0 +1,24 @@
+package com.scala.tutorials.d.functions.procedures
+
+/**
+  * Created by Ghazi Naceur on 25/10/2018.
+  */
+object Demo {
+  def main(args: Array[String]) {
+    printMe()
+    println("Returned Value : " + addInt(5, 7))
+  }
+
+  // function
+  def addInt(a: Int, b: Int): Int = {
+    var sum: Int = 0
+    sum = a + b
+
+    return sum
+  }
+
+  // procedure
+  def printMe( ) : Unit = {
+    println("Hello, Scala!")
+  }
+}
