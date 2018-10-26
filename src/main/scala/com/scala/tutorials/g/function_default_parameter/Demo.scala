7e8cf488e72fbc27b6e5c7e6a5105fd2ae6f8c42
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2018-10-26 21:07:34 +0100

    function default parameter

---
+++ src/main/scala/com/scala/tutorials/g/function_default_parameter/Demo.scala
@@ -0,0 +1,18 @@
+package com.scala.tutorials.g.function_default_parameter
+
+/**
+  * Created by Ghazi Naceur on 26/10/2018.
+  */
+
+object Demo {
+  def main(args: Array[String]) {
+    println("Returned Value : " + addInt())
+  }
+
+  def addInt(a:Int = 5, b:Int = 7) : Int = {
+    var sum: Int = 0
+    sum = a + b
+
+    return sum
+  }
+}
