f7172555426fb52b237ddcc59e3a7d2e38a30c6a
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2018-10-26 22:19:05 +0100

    doc for functions with named arguments

--- src/main/scala/com/scala/tutorials/j/functions_with_named_arguments/Demo.scala
+++ src/main/scala/com/scala/tutorials/j/functions_with_named_arguments/Demo.scala
@@ -3,6 +3,13 @@ package com.scala.tutorials.j.functions_with_named_arguments
 /**
   * Created by Ghazi Naceur on 26/10/2018.
   */
+
+/**
+  * In a normal function call, the arguments in the call are matched one by one in the
+  * order of the parameters of the called function. Named arguments allow you to pass
+  * arguments to a function in a different order. The syntax is simply that each argument
+  * is preceded by a parameter name and an equals sign.
+  */
 object Demo {
   def main(args: Array[String]) {
     printInt(b = 5, a = 7)
