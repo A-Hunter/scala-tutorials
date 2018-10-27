80a68c0ab4dfa74f620c8cace62422499ea1e6b7
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2018-10-27 14:26:09 +0100

    traits

---
+++ src/main/scala/com/scala/tutorials/r/traits/Demo.scala
@@ -0,0 +1,39 @@
+package com.scala.tutorials.r.traits
+
+/**
+  * Created by Ghazi Naceur on 27/10/2018.
+  */
+
+/**
+  * A trait encapsulates method and field definitions, which can then be reused by
+  * mixing them into classes. Unlike class inheritance, in which each class must
+  * inherit from just one superclass, a class can mix in any number of traits.
+  * Traits are used to define object types by specifying the signature of the supported
+  * methods. Scala also allows traits to be partially implemented but traits may not
+  * have constructor parameters.
+  * A trait definition looks just like a class definition except that it uses the
+  * keyword trait. The following is the basic example syntax of trait.
+  */
+trait Equal {
+  def isEqual(x: Any): Boolean
+  def isNotEqual(x: Any): Boolean = !isEqual(x)
+}
+
+class Point(xc: Int, yc: Int) extends Equal {
+  var x: Int = xc
+  var y: Int = yc
+
+  def isEqual(obj: Any) = obj.isInstanceOf[Point] && obj.asInstanceOf[Point].x == y
+}
+
+object Demo {
+  def main(args: Array[String]) {
+    val p1 = new Point(2, 3)
+    val p2 = new Point(2, 4)
+    val p3 = new Point(3, 3)
+
+    println(p1.isNotEqual(p2))
+    println(p1.isNotEqual(p3))
+    println(p1.isNotEqual(2))
+  }
+}
---
+++ src/main/scala/com/scala/tutorials/r/traits/Demo2.scala
@@ -0,0 +1,41 @@
+package com.scala.tutorials.r.traits
+
+/**
+  * Created by Ghazi Naceur on 27/10/2018.
+  */
+
+/**
+  * Value classes are new mechanism in Scala to avoid allocating runtime objects.
+  * It contains a primary constructor with exactly one val parameter. It contains
+  * only methods (def) not allowed var, val, nested classes, traits, or objects.
+  * Value class cannot be extended by another class. This can be possible by extending
+  * your value class with AnyVal. The typesafety of custom datatypes without the
+  * runtime overhead.
+  * Let us take an examples of value classes Weight, Height, Email, Age, etc.
+  * For all these examples it is not required to allocate memory in the application.
+  * A value class not allowed to extend traits. To permit value classes to extend traits,
+  * universal traits are introduced which extends for Any.
+  *
+  *
+  * When to Use Traits?
+  * There is no firm rule, but here are few guidelines to consider −
+  * If the behavior will not be reused, then make it a concrete class. It is not reusable
+  * behavior after all.
+  * If it might be reused in multiple, unrelated classes, make it a trait. Only traits can
+  * be mixed into different parts of the class hierarchy.
+  * If you want to inherit from it in Java code, use an abstract class.
+  * If you plan to distribute it in compiled form, and you expect outside groups to write
+  * classes inheriting from it, you might lean towards using an abstract class.
+  * If efficiency is very important, lean towards using a class.
+  */
+trait Printable extends Any {
+  def print(): Unit = println(this)
+}
+class Wrapper(val underlying: Int) extends AnyVal with Printable
+
+object Demo2 {
+  def main(args: Array[String]) {
+    val w = new Wrapper(3)
+    w.print() // actually requires instantiating a Wrapper instance
+  }
+}
