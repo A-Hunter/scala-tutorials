b2ab310fd6c270c3af9747cd06ace80b240fef74
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2018-10-27 09:39:39 +0100

    closures

---
+++ src/main/scala/com/scala/tutorials/o/closures/Demo.scala
@@ -0,0 +1,33 @@
+package com.scala.tutorials.o.closures
+
+/**
+  * Created by Ghazi Naceur on 27/10/2018.
+  */
+
+/**
+  * A closure is a function, whose return value depends on the value of one or more
+  * variables declared outside this function.
+  * The following piece of code with anonymous function.
+  * val multiplier = (i:Int) => i * 10
+  * Here the only variable used in the function body, i * 10 , is i, which is defined
+  * as a parameter to the function. Try the following code −
+  * val multiplier = (i:Int) => i * factor
+  * There are two free variables in multiplier: i and factor. One of them, i, is a
+  * formal parameter to the function. Hence, it is bound to a new value each time
+  * multiplier is called. However, factor is not a formal parameter, then what is this?
+  * Let us add one more line of code.
+  * var factor = 3
+  * val multiplier = (i:Int) => i * factor
+  * Now factor has a reference to a variable outside the function but in the enclosing
+  * scope. The function references factor and reads its current value each time.
+  * If a function has no external references, then it is trivially closed over itself.
+  * No external context is required.
+  */
+object Demo {
+  def main(args: Array[String]) {
+    println( "multiplier(1) value = " +  multiplier(1) )
+    println( "multiplier(2) value = " +  multiplier(2) )
+  }
+  var factor = 3
+  val multiplier = (i:Int) => i * factor
+}
