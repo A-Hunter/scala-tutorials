8ec1d8dded9cc504f79ae4ae6a5f8046e6389abf
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2018-11-02 22:07:04 +0100

    Match expressions

---
+++ src/main/scala/com/scala/tutorials/aa/expressions/match_expression/Demo.scala
@@ -0,0 +1,34 @@
+package com.scala.tutorials.aa.expressions.match_expression
+
+/**
+  * Created by Ghazi Naceur on 02/11/2018.
+  */
+
+/**
+  * This is a match expression example
+  */
+object Demo {
+
+  def main(args: Array[String]){
+
+    val day:String = "Mondakjy"
+
+    val week: Unit = day match{
+      case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" =>
+        "Work"
+        println("Work")
+
+      case "Saturday" | "Sunday" =>
+        "Weekend"
+        println("Weekend")
+
+      case other =>
+        println("The provided input is not a day")
+
+        //  We can write as well
+//      case _ =>
+//        println("a wildcard value")
+    }
+  }
+
+}
