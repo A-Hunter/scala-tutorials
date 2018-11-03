0ecf1c3fedeb3e8167f2db6ca2b43745509bfe7f
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2018-11-03 21:34:27 +0100

    Loop : while loop

---
+++ src/main/scala/com/scala/tutorials/c/loop/d/while_loop/Demo.scala
@@ -0,0 +1,16 @@
+package com.scala.tutorials.c.loop.d.while_loop
+
+/**
+  * Created by Ghazi Naceur on 03/11/2018.
+  */
+object Demo {
+
+  def main(args: Array[String]): Unit = {
+
+    var a = 1
+    while (a < 10){
+      println(a)
+      a+=1
+    }
+  }
+}
