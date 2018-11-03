95a97829c9ca184cd21691077ae19b1a19916589
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2018-11-03 11:54:38 +0100

    Loop : until vs to

--- src/main/scala/com/scala/tutorials/c/loop_breaks/ForLoop.scala
+++ src/main/scala/com/scala/tutorials/c/loop_breaks/ForLoop.scala
@@ -7,6 +7,14 @@ object ForLoop {
   def main(args: Array[String]) {
     var a = 0
 
+
+    for (a <- 1 until 10) yield println(a)
+
+    for (a <- 1 until 10) println(a)
+
+    // (a <- 1 until 10) ==> 1,2,3,4,5,6,7,8,9
+    // (a <- 1 to 10)    ==> 1,2,3,4,5,6,7,8,9,10
+
     // for loop execution with a range
     for (a <- 1 until 10) {
       println("Value of a: " + a)
