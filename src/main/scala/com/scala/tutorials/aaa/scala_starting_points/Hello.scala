ec844cc90e88a6db26503fb112f25b4850dc04d1
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2018-11-07 21:27:49 +0100

    Starting points in Scala

---
+++ src/main/scala/com/scala/tutorials/aaa/scala_starting_points/Hello.scala
@@ -0,0 +1,18 @@
+package com.scala.tutorials.aaa.scala_starting_points
+
+/**
+  * Created by Ghazi Naceur on 07/11/2018.
+  */
+
+// The first starting point : A scala object extending App Class
+object Hello extends App {
+
+  println("Hello")
+}
+
+// The second starting point : main procedure
+object Caller {
+  def main(args: Array[String]): Unit = {
+    println("Hello World ")
+  }
+}
