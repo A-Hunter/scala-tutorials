22e80e7e8f09068ffabb0b4fccfbc0d299b56154
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2018-11-15 21:19:27 +0100

    reduce and fold - foldLeft - foldRight

---
+++ src/main/scala/com/scala/tutorials/q/arrays/reducing_list_generic/Demo.scala
@@ -0,0 +1,26 @@
+package com.scala.tutorials.q.arrays.reducing_list_generic
+
+/**
+  * Created by Ghazi Naceur on 13/11/2018.
+  */
+object Demo extends App {
+
+  val numbers = List(1,2,3)
+  val ints = List(4,7,1,8,5)
+
+  println(numbers.fold(0)(_ + _))
+  // 0 is the starting value
+  // is similar to : numbers.sum
+  //is similar to  : numbers.fold(0)((x,y) => x+y)
+  println(numbers.fold(0)((x, y) => x + y))
+  println(numbers.sum)
+
+  println(ints.foldLeft(0)(_ - _)) // 0-4-7
+  println(ints.foldRight(0)(_ - _)) // 4-7
+  println(ints.foldRight(0)((x, y) => {
+    println(x)
+    println(y)
+    println("println(x-y)" + (x-y))
+    x - y
+  })) // 4-7
+}
