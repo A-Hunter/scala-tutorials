dedd97e8eb81fe7281722d62288c87fdea8a562d
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2018-11-22 22:20:50 +0100

    Mapping List

---
+++ src/main/scala/com/scala/tutorials/u/exception_handling/Converter.scala
@@ -0,0 +1,28 @@
+package com.scala.tutorials.u.exception_handling
+
+/**
+  * Created by Ghazi Naceur on 22/11/2018.
+  */
+object Converter extends App {
+
+  def toInt(s: String): Option[Int] ={
+    try {
+      Some(Integer.parseInt(s.trim))
+    } catch {
+      case e: Exception => None
+    }
+  }
+
+  println(toInt("5").get)
+  println(toInt("X"))
+
+  val strings = List("5","2","6","8","9","p")
+  val result1 = strings.map(toInt)
+  println(result1)
+
+  val result2 = strings.flatMap(toInt)
+  println(result2)
+
+  val result3 = strings.flatMap(toInt).sum
+  println(result3)
+}
---
+++ src/main/scala/com/scala/tutorials/u/exception_handling/Demo3.scala
@@ -0,0 +1,17 @@
+package com.scala.tutorials.u.exception_handling
+
+/**
+  * Created by Ghazi Naceur on 22/11/2018.
+  */
+object Demo3 extends App {
+
+  def list(v: Int) = List(v-1,v,v+1)
+
+  private val result: List[Int] = list(2)
+  println(result)
+  println(result.sum)
+
+  val numbers = List(1,2,3,4,5)
+  println(numbers.map(x => list(x)))
+  println(numbers.flatMap(x => list(x)))
+}
