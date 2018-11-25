7913ef1cc3f6544bdcce81740df50c6a7efa5e5c
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2018-11-25 19:19:17 +0100

    scala exercise

---
+++ src/main/scala/com/scala/execises/ex1/Ex1.scala
@@ -0,0 +1,74 @@
+package com.scala.execises.ex1
+
+import scala.util.control.Breaks
+
+/**
+  * Created by Ghazi Naceur on 25/11/2018.
+  */
+class Ex1 {
+
+  def evenNumber(number: Int): Boolean ={
+    number % 2 == 0
+  }
+
+  def exitsEvenNumber(list: List[Int]): Boolean ={
+
+      for (a <- list) {
+        if (a % 2 ==0) {
+          return true
+        }
+      }
+    false
+  }
+
+  def twistedSum(list: List[Integer]): Int ={
+    var sum: Integer = 0
+
+    list.foreach(x => {
+      if (x == 7) {
+        sum += 14
+      } else {
+        sum += x
+      }
+    })
+    sum
+  }
+
+  def canBeEquallySplit(list: List[Int]): Boolean ={
+    var canBeSplit: Boolean = false
+    var half: Int = 0
+    val loop = new Breaks
+
+    loop.breakable {
+      for (a <- list) {
+        half += a
+        if (half == list.sum / 2) {
+          canBeSplit = true
+          loop.break
+        }
+      }
+    }
+    canBeSplit
+  }
+
+  def canBeEquallySplitV2 (list: List[Int]): Boolean ={
+
+    var firstHalf = 0
+    var secondHalf = 0
+    secondHalf = list.sum
+
+    for (i <- list.indices){
+      firstHalf += list(i)
+      secondHalf -= list(i)
+
+      if (firstHalf == secondHalf){
+        return true
+      }
+    }
+    false
+  }
+
+  def palindrome(string: String): Boolean ={
+    string.reverse.equals(string)
+  }
+}
---
+++ src/main/scala/com/scala/execises/ex1/Ex1Main.scala
@@ -0,0 +1,36 @@
+package com.scala.execises.ex1
+
+/**
+  * Created by Ghazi Naceur on 25/11/2018.
+  */
+object Ex1Main extends App {
+
+  private val ex = new Ex1
+
+  println("Even number :")
+  println(ex.evenNumber(2))
+  println(ex.evenNumber(3))
+
+  println("Even number in list :")
+  println(ex.exitsEvenNumber(List(1, 2, 3)))
+  println(ex.exitsEvenNumber(List(1, 3, 3)))
+
+  println("Twisted sum :")
+  println(ex.twistedSum(List(1, 7, 3)))
+  println(ex.twistedSum(List(1, 7, 7)))
+  println(ex.twistedSum(List(1, 2, 3)))
+
+  println("Equally split list :")
+  println(ex.canBeEquallySplit(List(1, 2, 3)))
+  println(ex.canBeEquallySplit(List(1, 3, 3, 4 ,2, 13)))
+  println(ex.canBeEquallySplit(List(1, 2, 3, 4)))
+
+  println("Equally split list V2 :")
+  println(ex.canBeEquallySplitV2(List(1, 2, 3)))
+  println(ex.canBeEquallySplitV2(List(1, 3, 3, 4 ,2, 13)))
+  println(ex.canBeEquallySplitV2(List(1, 2, 3, 4)))
+
+  println("Palindrome :")
+  println(ex.palindrome("text"))
+  println(ex.palindrome("texet"))
+}
