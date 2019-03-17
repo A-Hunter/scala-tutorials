276fd7cdcb1dbadf05a1b5ad0e100813fa1d04ed
A-Hunter <ghazi.ennacer@gmail.com>
2019-03-17 18:47:23 +0100

    Higher order function : new example

--- src/main/scala/com/scala/tutorials/l/higher_order_functions/Demo.scala
+++ src/main/scala/com/scala/tutorials/l/higher_order_functions/Demo.scala
@@ -13,9 +13,21 @@ package com.scala.tutorials.l.higher_order_functions
 object Demo {
   def main(args: Array[String]) {
     println(apply(layout, 10))
+    println(apply(layout2, 50))
+    println(doSomething(go, 1, 2, 3))
   }
 
   def apply(f: Int => String, v: Int): String = f(v)
 
   def layout[A](x: A): String = "[" + x.toString + "]"
+
+  def layout2(v: Int): String = {
+    v.toString
+  }
+
+  def doSomething(f: (Int, Int, Int) => Int, x: Int, y: Int, z: Int): Int = f(x, y, z)
+
+  def go(x: Int, y: Int, z: Int): Int = {
+    (x + y) * z
+  }
 }
