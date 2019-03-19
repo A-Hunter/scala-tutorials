e130cd937e01e2265f7efbd1c9b2d0142f625f03
A-Hunter <ghazi.ennacer@gmail.com>
2019-03-19 23:24:25 +0100

    Traits

---
+++ src/main/scala/com/scala/lang/org/traits/Iterator.scala
@@ -0,0 +1,42 @@
+package com.scala.lang.org.traits
+
+/**
+  * Created by Ghazi Naceur on 19/03/2019
+  * Email: ghazi.ennacer@gmail.com
+  */
+trait Iterator[A] {
+  def hasNext: Boolean
+
+  def next(): A
+}
+
+class IntIterator(limit: Int) extends Iterator[Int] {
+  var current: Int = 0
+
+  override def hasNext: Boolean = current < limit
+
+  override def next(): Int = {
+    if (hasNext) {
+      val iterator = current
+      current += 1
+      iterator
+    } else 0
+  }
+}
+
+object Iterator {
+  def main(args: Array[String]): Unit = {
+    val iterator = new IntIterator(10)
+    println(iterator.next()) // prints 0
+    println(iterator.next()) // prints 1
+    println(iterator.next()) // prints 2
+    println(iterator.next()) // prints 3
+    println(iterator.next()) // prints 4
+    println(iterator.next()) // prints 5
+    println(iterator.next()) // prints 6
+    println(iterator.next()) // prints 7
+    println(iterator.next()) // prints 8
+    println(iterator.next()) // prints 9
+    println(iterator.next()) // prints 0
+  }
+}
---
+++ src/main/scala/com/scala/lang/org/traits/Pet.scala
@@ -0,0 +1,29 @@
+package com.scala.lang.org.traits
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  * Created by Ghazi Naceur on 19/03/2019
+  * Email: ghazi.ennacer@gmail.com
+  */
+trait Pet {
+  val name: String
+}
+
+// "name" is overridden, this time, in the constructor, not in the body of the class
+class Dog(val name: String) extends Pet
+class Cat(val name: String) extends Pet
+
+object Pet{
+  def main(args: Array[String]): Unit = {
+
+    val dog = new Dog("Rex")
+    val cat = new Dog("Tom")
+    val pets = ArrayBuffer.empty[Pet]
+
+    pets.append(dog)
+    pets.append(cat)
+
+    pets.foreach(pet => println(pet.name))
+  }
+}
