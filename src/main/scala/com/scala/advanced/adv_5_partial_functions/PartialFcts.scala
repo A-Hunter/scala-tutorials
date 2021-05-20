e644b59775916a84b0badd474b1cf3334af40eb1
ghazi-naceur <ghazi.ennacer@gmail.com>
2021-05-20 16:59:01 +0200

    Rename package

--- src/main/scala/com/scala/advanced/adv_5_partial_functions/PartialFcts.scala
+++ src/main/scala/com/scala/advanced/adv_5_partial_functions/PartialFcts.scala
@@ -1,17 +1,17 @@
-package com.scala.advanced.partial_functions
+package com.scala.advanced.adv_5_partial_functions
 
 object PartialFcts extends App {
 
   val bot= new PartialFunction[String, String] {
     override def apply(x: String): String = x match {
-      case "how are you ?" => "I am bot"
+      case "who are you ?" => "I am bot"
       case "version ?" => "1.0"
       case "date ?" => "28/04/2021"
       case "status ?" => "running"
     }
 
     override def isDefinedAt(x: String): Boolean =
-      x == "how are you ?" || x == "version ?" || x == "date ?" || x == "status ?"
+      x == "who are you ?" || x == "version ?" || x == "date ?" || x == "status ?"
   }
 
   scala.io.Source.stdin.getLines().map(bot).foreach(println)
