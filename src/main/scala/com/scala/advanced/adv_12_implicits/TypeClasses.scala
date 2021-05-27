8c85c2a828a9def05d7205056be8b3d410d0854f
Ghazi Naceur <ghazi.ennacer@gmail.com>
2021-05-27 13:34:27 +0200

    Implicits for type classes

--- src/main/scala/com/scala/advanced/adv_12_implicits/TypeClasses.scala
+++ src/main/scala/com/scala/advanced/adv_12_implicits/TypeClasses.scala
@@ -48,11 +48,49 @@ object TypeClasses extends App {
     def equal(value1: T, value2: T): Boolean
   }
 
-  object NameEquality extends Equal[User] {
+  implicit object NameEquality extends Equal[User] {
     override def equal(value1: User, value2: User): Boolean = value1.name == value2.name
   }
 
   object FullEquality extends Equal[User] {
     override def equal(value1: User, value2: User): Boolean = value1.name == value2.name && value1.email == value2.email
   }
+
+  object HTMLSerializer {
+    def serialize[T](value: T)(implicit serializer: HTMLSerializer[T]): String =
+      serializer.serialize(value)
+
+    // surface out the serializer with an apply method => Access to the entire type class interface
+    def apply[T](implicit serializer: HTMLSerializer[T]) = serializer
+  }
+
+  implicit object IntSerializer extends HTMLSerializer[Int] {
+    override def serialize(value: Int): String = s"<div>Integer: $value</div>"
+  }
+
+  println(HTMLSerializer.serialize(User("Isaac", 125, "isaac.netero@hunter.com"))(UserSerializer))
+  println(HTMLSerializer.serialize(56)) // IntSerializer is implicit not like UserSerializer, so no need to provide it as a parameter
+                                              // The compiler will figure out that we need to use the IntSerializer implicitly
+  println(HTMLSerializer[Int].serialize(56)) // We're apply to specify the Type "Int" here, because we're using "apply" method
+  // inside the companion object, which surfaced out the serializer (the implicit instance) => Access to the entire type class interface
+
+  trait MyTypeClassTemplate[T] {
+    def action(value: T): String
+  }
+
+  object MyTypeClassTemplate {
+    def apply[T](implicit instance: MyTypeClassTemplate[T]) = instance
+  }
+
+  object Equal {
+    def apply[T](a: T, b: T)(implicit equalizer: Equal[T]): Boolean = equalizer.equal(a, b)
+  }
+
+  private val netero: User = User("Netero", 125, "isaac.netero@hunter.com")
+
+  println(Equal.apply(netero, netero))
+  println(Equal(netero, netero))
+  // => This is an AD-HOC Polymorphism : When calling Equal(..), the compiler will try to find the appropriate equalizer
+  // depending on the type of the parameters (User in our case)
+
 }
